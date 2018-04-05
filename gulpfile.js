01cf511c40485b7006a9038939f7a65f50a30324
Tharindu <tharinduyasarathna@gmail.com>
2018-04-05 23:11:23 +0530

    adding gulpplugins

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autpfrefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested');
+
+gulp.task('default', function(){
+    console.log("gulf default task.");
+});
+
+gulp.task('html', function(){
+    console.log("Html gulf task.");
+});
+
+gulp.task('styles', function(){
+    return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([ cssvars ,nested, autpfrefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('watch', function(){
+
+    watch('./app/index.html', function(){
+        gulp.start('html');
+    });
+    
+        watch('./app/assets/styles/**/*.css', function(){
+            gulp.start('styles');
+        });
+
+});
