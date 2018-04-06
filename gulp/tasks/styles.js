ad1567bd5487cff29a69df3c676d2ea62d3085ad
Tharindu <tharinduyasarathna@gmail.com>
2018-04-06 17:41:45 +0530

    add Mobile-first Essentials

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,11 +3,12 @@ postcss = require('gulp-postcss'),
 autpfrefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssimport = require('postcss-import');
+cssimport = require('postcss-import'),
+mixins = require('postcss-mixins');
 
 gulp.task('styles', function(){
     return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([ cssimport,cssvars ,nested, autpfrefixer]))
+    .pipe(postcss([ cssimport,mixins,cssvars ,nested, autpfrefixer]))
     .on('error', function(errorInfo){
         console.log(errorInfo.toString());
         this.emit('end');
